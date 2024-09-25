b754b5e01963eabbfbbd0fac4e28827fca9dab8b
Dushyant <dushyant.iitj@gmail.com>
2024-09-25 16:56:36 +0530

    refactor: minor updates

--- src/components/strandsGrid/StrandButton.tsx
+++ src/components/strandsGrid/StrandButton.tsx
@@ -10,6 +10,10 @@ const StrandButton = ({ alphabet, buttonSize, onClick }: StrandButtonProps) => {
   return (
     <button
       className={`w-[${buttonSize}px] h-[${buttonSize}px] rounded-full hover:shadow-lg shadow-black/50 uppercase text-xl transition-all duration-50 ease-in-out`}
+      style={{
+        height: `${buttonSize}px`,
+        width: `${buttonSize}px`
+      }}
       onClick={onClick}
     >
       {alphabet}
